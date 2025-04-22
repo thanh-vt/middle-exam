b59eea59d643a31809cd923eb6a5c49418e90b7a
pysga1996 <pysga1996@gmail.com>
2025-04-22 22:03:45 +0700

    init #2

--- FPLDLW25638FTP90.c
+++ FPLDLW25638FTP90.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+void printBinary(int x);
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1 || n < 1) {
+        printf("Vui lòng nhập số nguyên dương.\n");
+        return 1;
+    }
+
+    printf("Bảng nhị phân từ 1 đến %d:\n", n);
+    for (int i = 1; i <= n; i++) {
+        printBinary(i);
+        putchar('\n');
+    }
+
     return 0;
+}
+
+void printBinary(int x) {
+    if (x > 1) {
+        printBinary(x / 2);
+    }
+    putchar((x % 2) + '0');
 }
